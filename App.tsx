a795625e709b982d1046ee240fbf25fb5fef6ac1
Eerik Pitkänen <eerikpit@gmail.com>
2026-02-18 21:19:02 +0200

    valmis

--- App.tsx
+++ App.tsx
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [torch, setTorch] = useState(false);
+  const [result, setResult] = useState("")
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleTorch() {
+    setTorch(current => (current === false ? true : false));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView
+        style={styles.camera}
+        facing="back"
+        enableTorch={torch} 
+        onBarcodeScanned={(scanningResult) => {
+          setResult(scanningResult.data)
+        }}
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr", "ean13", "code128"],
+        }}
+      />
+      <View style={styles.resultContainer}>
+        <Text style={styles.text}>Barcode: {result}</Text> 
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleTorch}>
+          <Text style={styles.text}>Enable Torch</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +54,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 64,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    width: '100%',
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  resultContainer: {
+  position: 'absolute',
+  bottom: 140,
+  width: '100%',
+  alignItems: 'center',
   },
 });
